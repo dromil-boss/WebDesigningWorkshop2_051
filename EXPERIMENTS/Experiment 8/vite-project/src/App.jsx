c4d4aceeff6fd8ff44685c0d4bb94b7f0687429f
dromil-boss <dromilsharma@gmail.com>
2026-05-13 11:10:21 +0530

    pp

---
+++ EXPERIMENTS/Experiment 8/vite-project/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <div className="container">
+      <h1>React Counter App</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
